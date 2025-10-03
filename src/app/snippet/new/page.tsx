8730a498714f5e127e0869a4868e5014bfdd1c17
saha-7op <saha7pritam@gmail.com>
2025-10-04 00:13:00 +0530

    prismaClient

--- src/app/snippet/new/page.tsx
+++ src/app/snippet/new/page.tsx
@@ -1,9 +1,17 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
+import { prisma } from '@/lib/prisma'
 import React from 'react'
 
 const page = () => {
+    async function createSnippet(formdata: FormData){
+        "use server" // this is a server action
+        const title = formdata.get("title") as string
+        const code = formdata.get("code") as string
+
+        await prisma.snippet.create({})
+    }
   return (
     <form className='flex flex-col gap-10'>
     <div className='flex gap-4'>
