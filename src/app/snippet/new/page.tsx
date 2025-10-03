a0a9286546ab2ee42c8dcaf518a82a3746d50223
saha-7op <saha7pritam@gmail.com>
2025-10-03 22:22:42 +0530

    Prisma migrate

--- src/app/snippet/new/page.tsx
+++ src/app/snippet/new/page.tsx
@@ -1,10 +1,21 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      New Snippet
+    <form className='flex flex-col gap-10'>
+    <div className='flex gap-4'>
+      <label>Title</label>
+      <Input type='text' name='title' placeholder='Title' id='title'/>
     </div>
+    <div className='flex gap-4'>
+      <label>Code</label>
+      <Textarea id='code' name='code' placeholder='Add code here...'  />
+    </div>
+    <Button type="submit" className='my-6 w-20'>New</Button>
+    </form>
   )
 }
 
