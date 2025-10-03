20579d68bb97ec44007f5ecadd1548d69f5c1316
saha-7op <saha7pritam@gmail.com>
2025-10-03 21:35:27 +0530

    new snippet routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <h1 className="font-bold text-4xl">Home</h1>
+      <div className="flex justify-between items-center">
+        <h1>Snippet</h1>
+        <Link href="/snippet/new"><Button>New</Button></Link>
+      </div>
+    </div>
   );
 }
---
+++ src/app/snippet/new/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      New Snippet
+    </div>
+  )
+}
+
+export default page
